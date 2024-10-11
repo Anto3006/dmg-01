95245d01ae2f2a20c1b2579d3d7ceca2613aeb03
Antonio Piedra <anto.piedrapach@hotmail.com>
2024-10-10 21:05:57 -0600

    Implemented ADC, SUB and SBC ADC: addition with carry SUB: substraction SBC: substraction with carry

--- src/cpu.rs
+++ src/cpu.rs
@@ -14,6 +14,9 @@ enum ArithmeticTarget {
 
 enum Instruction {
     ADD(ArithmeticTarget),
+    ADC(ArithmeticTarget),
+    SUB(ArithmeticTarget),
+    SBC(ArithmeticTarget),
 }
 
 struct CPU {
@@ -23,19 +26,59 @@ struct CPU {
 impl CPU {
     fn execute(&mut self, instruction: Instruction) {
         match instruction {
-            Instruction::ADD(target) => self.add(target),
+            Instruction::ADD(target) => self.add(target, false),
+            Instruction::ADC(target) => self.add(target, true),
+            Instruction::SUB(target) => self.sub(target, false),
+            Instruction::SBC(target) => self.sub(target, true),
             _ => (),
         }
     }
 
-    fn add(&mut self, target: ArithmeticTarget) {
+    fn add(&mut self, target: ArithmeticTarget, use_carry: bool) {
         let value = self.get_target_value(target);
-        let (new_value, did_overflow) = self.registers.a.overflowing_add(value);
+        let (mut new_value, mut did_overflow) = self.registers.a.overflowing_add(value);
+        if use_carry {
+            let (new_value_carry, did_overflow_carry) =
+                new_value.overflowing_add(self.registers.f.carry as u8);
+            new_value = new_value_carry;
+            did_overflow = did_overflow || did_overflow_carry;
+        }
         self.registers.f.zero = new_value == 0;
         self.registers.f.substract = false;
-        self.registers.f.half_carry = (self.registers.a & 0x0F) + (value & 0x0F) > 0x0F;
+        self.registers.f.half_carry = self.did_half_carry_add(self.registers.a, value, use_carry);
         self.registers.f.carry = did_overflow;
         self.registers.a = new_value;
+    }
+
+    fn did_half_carry_add(&self, value1: u8, value2: u8, use_carry: bool) -> bool {
+        if !use_carry {
+            ((value1 & 0x0F) + (value2 & 0x0F)) > 0x0F
+        } else {
+            ((value1 & 0x0F) + (value2 & 0x0F)) + (self.registers.f.carry as u8) > 0x0F
+        }
+    }
+
+    fn sub(&mut self, target: ArithmeticTarget, use_carry: bool) {
+        let value = self.get_target_value(target);
+        let (mut new_value, mut did_borrow) = self.registers.a.overflowing_sub(value);
+        if use_carry {
+            let (new_value_carry, did_borrow_carry) =
+                new_value.overflowing_sub(self.registers.f.carry as u8);
+            new_value = new_value_carry;
+            did_borrow = did_borrow || did_borrow_carry;
+        }
+        self.registers.f.zero = new_value == 0;
+        self.registers.f.substract = true;
+        self.registers.f.half_carry = self.did_half_carry_sub(self.registers.a, value, use_carry);
+        self.registers.f.carry = did_borrow;
+    }
+
+    fn did_half_carry_sub(&self, value1: u8, value2: u8, use_carry: bool) -> bool {
+        if !use_carry {
+            (value1 & 0x0F) < (value2 & 0x0F)
+        } else {
+            (value1 & 0x0F) < ((value2 & 0x0F) + (self.registers.f.carry as u8))
+        }
     }
 
     fn get_target_value(&self, target: ArithmeticTarget) -> u8 {
